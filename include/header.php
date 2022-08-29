e16b97532ec05927f70e89ed9a0800297a4d26b7
hyunjinP <wldwlddl0531@gmail.com>
2022-08-29 14:34:51 +0900

    2022-08-29

--- include/header.php
+++ include/header.php
@@ -11,6 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/mall/image/favicon.ico" />
     <title>GreenBooks</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="/mall/css/style.css">
